d42f1e093f201488a70000f18ad233f473e401d8
ngwueche <wisdomngwueche@gmail.com>
2023-12-17 22:23:10 +0100

    added AppUser Model

---
+++ BookWorm.Models/Models/AppUser.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace BookWorm.Models
+{
+    public class AppUser
+    {
+        public int Id { get; set; }
+        [Required]
+        [Length(3, 30)]
+        [DisplayName("Name")]
+        public string Name { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+        public DateTime UpdatedAt { get; set; } = DateTime.UtcNow;
+        public DateTime DeletedAt { get; set; } = DateTime.UtcNow;
+
+    }
+}
