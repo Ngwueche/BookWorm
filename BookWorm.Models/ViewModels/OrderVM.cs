45c27f97ff1b6f8c4f2c0ba5cc9113b0def69761
ngwueche <wisdomngwueche@gmail.com>
2024-01-04 01:10:03 +0100

    implemented order filter

--- BookWorm.Models/ViewModels/OrderVM.cs
+++ BookWorm.Models/ViewModels/OrderVM.cs
@@ -5,6 +5,6 @@ namespace BookWorm.Models.ViewModels
     public class OrderVM
     {
         public OrderHeader OrderHeader { get; set; }
-        public IEnumerable<OrderDetail> OrderDetail { get; }
+        public IEnumerable<OrderDetail> OrderDetail { get; set; }
     }
 }
--- BookWorm/Areas/Admin/Controllers/OrderController.cs
+++ BookWorm/Areas/Admin/Controllers/OrderController.cs
@@ -1,5 +1,7 @@
 ﻿using BookWorm.DataAccess.Repositories.IRepositories;
 using BookWorm.Models.Models;
+using BookWorm.Models.ViewModels;
+using BookWorm.Utility;
 using Microsoft.AspNetCore.Mvc;
 
 namespace BookWorm.API.Areas.Admin.Controllers
@@ -16,12 +18,40 @@ namespace BookWorm.API.Areas.Admin.Controllers
         {
             return View();
         }
+        public IActionResult Details(int orderId)
+        {
+            OrderVM orderVM = new()
+            {
+                OrderHeader = _unitOfWork.OrderHeaderRepository.Get(u => u.Id == orderId, includeProperties: "ApplicationUser"),
+                OrderDetail = _unitOfWork.OrderDetailRepository.GetAll(u => u.OrderHeader.Id == orderId, includeProperties: "Product")
+            };
+            return View(orderVM);
+        }
 
         #region API CALLS
         [HttpGet]
-        public IActionResult GetAll()
+        public IActionResult GetAll(string status)
         {
-            List<OrderHeader> orderHeaders = _unitOfWork.OrderHeaderRepository.GetAll(includeProperties: "ApplicationUser").ToList();
+            IEnumerable<OrderHeader> orderHeaders = _unitOfWork.OrderHeaderRepository.GetAll(includeProperties: "ApplicationUser").ToList();
+
+            switch (status)
+            {
+                case "pending":
+                    orderHeaders = orderHeaders.Where(u => u.OrderStatus == SD.PaymentStatusDelayedPayment);
+                    break;
+                case "inprocess":
+                    orderHeaders = orderHeaders.Where(u => u.OrderStatus == SD.StatusInProcess);
+                    break;
+                case "approved":
+                    orderHeaders = orderHeaders.Where(u => u.OrderStatus == SD.StatusApproved);
+                    break;
+                case "completed":
+                    orderHeaders = orderHeaders.Where(u => u.OrderStatus == SD.StatusShipped);
+                    break;
+                default:
+                    break;
+
+            }
             return Json(new { data = orderHeaders });
         }
         #endregion
