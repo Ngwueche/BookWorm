84d771d53b4094a785de3cf7d7df068a0a4d671b
ngwueche <wisdomngwueche@gmail.com>
2024-01-05 18:17:16 +0100

    stripe payment for company

--- BookWorm/Areas/Admin/Controllers/OrderController.cs
+++ BookWorm/Areas/Admin/Controllers/OrderController.cs
@@ -5,6 +5,7 @@ using BookWorm.Utility;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Stripe;
+using Stripe.Checkout;
 using System.Security.Claims;
 
 namespace BookWorm.API.Areas.Admin.Controllers
@@ -106,6 +107,65 @@ namespace BookWorm.API.Areas.Admin.Controllers
             TempData["success"] = "Your Order Cancelled";
             return RedirectToAction(nameof(Details), new { orderId = OrderVM.OrderHeader.Id });
         }
+        [ActionName(nameof(Details))]
+        [HttpPost]
+        public IActionResult Details_Pay_Now()
+        {
+            OrderVM.OrderHeader = _unitOfWork.OrderHeaderRepository.Get(u => u.Id == OrderVM.OrderHeader.Id, includeProperties: "ApplicationUser");
+            OrderVM.OrderDetail = _unitOfWork.OrderDetailRepository.GetAll(u => u.Id == OrderVM.OrderHeader.Id, includeProperties: "Product");
+            //stripe payment GateWay integration
+            string domain = "https://localhost:7174/";
+            var options = new Stripe.Checkout.SessionCreateOptions
+            {
+                SuccessUrl = domain + $"admin/order/PaymentConfirmation?orderHeaderId={OrderVM.OrderHeader.Id}",
+                CancelUrl = domain + $"admin/order/details?orderId={OrderVM.OrderHeader.Id}",
+                LineItems = new List<Stripe.Checkout.SessionLineItemOptions>(),
+                Mode = "payment",
+            };
+            foreach (var item in OrderVM.OrderDetail)
+            {
+                SessionLineItemOptions sessionLineItemOptions = new SessionLineItemOptions
+                {
+                    PriceData = new SessionLineItemPriceDataOptions
+                    {
+                        UnitAmount = (long)item.Price * 100,
+                        Currency = "usd",
+                        ProductData = new SessionLineItemPriceDataProductDataOptions
+                        {
+                            Name = item.Product.Title,
+                        }
+                    },
+                    Quantity = item.Count
+                };
+                options.LineItems.Add(sessionLineItemOptions);
+            }
+            var service = new Stripe.Checkout.SessionService();
+            Session session = service.Create(options);
+            _unitOfWork.OrderHeaderRepository.UpdateStripePaymentId(OrderVM.OrderHeader.Id, session.Id, session.PaymentIntentId);
+            _unitOfWork.Save();
+            Response.Headers.Location = session.Url;
+            return new StatusCodeResult(303);
+        }
+
+        public IActionResult PaymentConfirmation(int orderHeaderId)
+        {
+            OrderHeader orderHeader = _unitOfWork.OrderHeaderRepository.Get(u => u.Id == orderHeaderId, includeProperties: "ApplicationUser");
+            if (orderHeader.PaymentStatus == SD.PaymentStatusDelayedPayment)
+            {
+                //This checks for customer payment
+                SessionService sessionService = new SessionService();
+                Session session = sessionService.Get(orderHeader.SessionId);
+                if (session.PaymentStatus.Equals("paid", StringComparison.CurrentCultureIgnoreCase))
+                {
+                    _unitOfWork.OrderHeaderRepository.UpdateStripePaymentId(orderHeaderId, session.Id, session.PaymentIntentId);
+                    _unitOfWork.OrderHeaderRepository.UpdateStatus(orderHeaderId, orderHeader.OrderStatus, SD.PaymentStatusApproved);
+                    _unitOfWork.Save();
+                }
+            };
+
+            return View(orderHeaderId);
+        }
+
         //[HttpPost]
         //public IActionResult ShipOrder()
         //{
