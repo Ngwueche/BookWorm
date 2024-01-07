08d6859b6f2fc8baeb32fc0da8023ac70135caba
ngwueche <wisdomngwueche@gmail.com>
2024-01-07 01:43:56 +0100

    sendGrid setup

--- BookWorm.Utilities/EmailSender.cs
+++ BookWorm.Utilities/EmailSender.cs
@@ -1,12 +1,24 @@
 ﻿using Microsoft.AspNetCore.Identity.UI.Services;
+using Microsoft.Extensions.Configuration;
+using SendGrid;
+using SendGrid.Helpers.Mail;
 
 namespace BookWorm.Utilities
 {
     public class EmailSender : IEmailSender
     {
+        private string SendGridSecretKey { get; set; }
+        public EmailSender(IConfiguration _config)
+        {
+            SendGridSecretKey = _config.GetValue<string>("SendGrid:SecretKey");
+        }
         public Task SendEmailAsync(string email, string subject, string htmlMessage)
         {
-            return Task.CompletedTask;
+            var client = new SendGridClient(SendGridSecretKey);
+            var from = new EmailAddress("hello@bookworm.com");
+            var to = new EmailAddress(email);
+            var message = MailHelper.CreateSingleEmail(from, to, subject, "", htmlMessage);
+            return client.SendEmailAsync(message);
         }
     }
 }
